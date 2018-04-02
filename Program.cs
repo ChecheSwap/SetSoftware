4acfc833918b3dbb69bc0319e4f457515f4cdf1c
Jesús José Navarrete Baca <chechenb19972@hotmail.com>
2018-04-02 02:47:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MundoMusical;
+using MundoMusical.CUSTOMER;
+using MundoMusical.PRODUCT;
+using MundoMusical.CATEGORY;
+using MundoMusical.ATOM;
+using MundoMusical.PAYMODE;
+using MundoMusical.VENTA;
+using MundoMusical.CONFIGURACION_DE_IMPRESION;
+using MundoMusical.INVENTARIO;
+using MundoMusical.XBASE;
+using MundoMusical.TIENDA;
+using MundoMusical.DB.DUMPS;
+using MundoMusical.CONSULTAS.CLIENTES;
+using MundoMusical.CONSULTAS;
+using MundoMusical.CONSULTAS.VENTAS;
+using MundoMusical.TICKET.CRYSTAL_RPT_TICKET.XML_SCHEMA;
+using MundoMusical.CONSULTAS.INICIOS_SESION;
+using System.Threading;
+using System.Globalization;
+using MundoMusical.LABELS;
+
+namespace MundoMusical
+{
+    static class Program
+    {
+        /// <summary>
+        // Code By::JESUS JOSE NAVARRETE BACA 
+        /// </summary>
+        /// 
+        [STAThread]
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new initForm());
+        }
+    }
+}
